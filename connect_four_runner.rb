ac50ca28affeef12f30be4716878a9d4dc1dfad9
Matt Deming <vballmatt44@gmail.com>
2022-03-31 14:15:01 -0600

    feat: add runner file

---
+++ connect_four_runner.rb
@@ -0,0 +1,27 @@
+require './lib/board'
+require './lib/player'
+require 'pry'
+
+# binding.pry
+
+
+board.start
+board.game_intro
+
+
+
+
+
+
+
+# class Player
+#   p "Player Wins! Fatality!"
+# end
+#
+# class Computer
+#   p "You buggin'! Computer Always Wins!
+# end
+#
+# class Game
+#   p "It's a Draw! Everybody sit down and STFU!"
+# end
