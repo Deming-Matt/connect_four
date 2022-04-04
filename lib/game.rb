3a6792a78444b70c6eabea6f427cb2b0f421eaec
Matt Deming <vballmatt44@gmail.com>
2022-04-04 13:31:24 -0600

    To be deleted

---
+++ lib/game.rb
@@ -0,0 +1,13 @@
+require './lib/board'
+require './lib/player'
+require './lib/computer'
+require 'pry'
+
+class Game
+
+# "Here is the board."
+# (print current board_string)
+# "whicn column would you like to place your piece?"
+# user_input for column pick
+
+end
